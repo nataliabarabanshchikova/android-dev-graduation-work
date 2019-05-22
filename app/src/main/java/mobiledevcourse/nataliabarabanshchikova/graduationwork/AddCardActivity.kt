fdd1bd11fab4a544fc33114862e467b82981b096
Natalia Barabanschikova <natalia.barabanschikova@redsolution.ru>
2019-05-22 18:07:11 +0500

    Added API request to create new card

--- app/src/main/java/mobiledevcourse/nataliabarabanshchikova/graduationwork/AddCardActivity.kt
+++ app/src/main/java/mobiledevcourse/nataliabarabanshchikova/graduationwork/AddCardActivity.kt
@@ -3,8 +3,20 @@ package mobiledevcourse.nataliabarabanshchikova.graduationwork
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.widget.EditText
+import kotlinx.coroutines.*
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import kotlin.coroutines.CoroutineContext
 
-class AddCardActivity : AppCompatActivity() {
+class AddCardActivity : AppCompatActivity(), CoroutineScope {
+
+    private val httpClient = OkHttpClient.Builder().build()
+
+    private val rootJob = Job()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + rootJob
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +30,9 @@ class AddCardActivity : AppCompatActivity() {
 
         val btnAddCard = findViewById<Button>(R.id.btnAddCard)
         btnAddCard.setOnClickListener {
-            addCard(listId)
+            val newCardName = findViewById<EditText>(R.id.cardName).text.toString()
+            val newCardDesc = findViewById<EditText>(R.id.cardDesc).text.toString()
+            addCard(listId, newCardName, newCardDesc)
         }
     }
 
@@ -27,5 +41,29 @@ class AddCardActivity : AppCompatActivity() {
         return true
     }
 
-    private fun addCard(listId: String) {}
+    private fun addCard(listId: String, name: String, desc: String) = launch {
+        val apiKey = MyApplication.prefs!!.apiKey
+        val apiToken = MyApplication.prefs!!.apiToken
+        val builder = FormBody.Builder()
+        builder.add("name", name)
+        builder.add("desc", desc)
+        val formBody = builder.build()
+        val request = Request.Builder()
+            .url("https://api.trello.com/1/cards/?" +
+                    "idList=$listId&keepFromSource=all&" +
+                    "key=$apiKey&" +
+                    "token=$apiToken")
+            .post(formBody)
+            .build()
+        val response: String = withContext(Dispatchers.IO) {
+            httpClient.newCall(request).execute().body()!!.string()
+        }
+        finish()
+    }
+
+    override fun onDestroy() {
+        rootJob.cancel()
+        super.onDestroy()
+    }
+
 }
